9c5f80651e22d5679ec8dfd9b42a1d1b36832f3d
Myrkur <Myrkur@myrkur.dev>
2022-08-20 09:46:31 -0400

    (Major) VCS Initialize - Initial Commit

---
+++ WPF OSC Keyboard/ConsoleManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace WPF_OSC_Keyboard
+{
+    public static class ConsoleManager
+    {
+        [DllImport("kernel32.dll")]
+        private static extern int AllocConsole();
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+
+        private static void ShowConsole()
+        {
+            SetForegroundWindow(GetConsoleWindow());
+        }
+
+        public static void ConsoleInitalize()
+        {
+            AllocConsole();
+            Console.SetOut(new StreamWriter(Console.OpenStandardOutput())
+            {
+                AutoFlush = true
+            });
+            Console.SetIn(new StreamReader(Console.OpenStandardInput()));
+            Console.Clear();
+            ShowConsole();
+        }
+    }
+}
---
+++ WPF OSC Keyboard/MainWindow.xaml.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Rug.Osc;
+
+namespace WPF_OSC_Keyboard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static string Inputtextbox = "";
+        public static bool TextPopulated;
+        private static MainWindow form;
+        private int _textpos = 0;
+        public MainWindow()
+        {
+            form = this;
+            InitializeComponent();
+            InputText.KeyDown += KeydownAction;
+            InputText.TextChanged += TextChangedText;
+            ChatBox.TextChanged += TextChanged;
+            Console.WriteLine("ConsoleInitialized");
+            OSCData.start();
+
+        }
+
+        private void Window_PreviewLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            var window = (Window)sender;
+            window.Topmost = true;
+        }
+        
+        private void TextChangedText(object sender, RoutedEventArgs e)
+        {
+            Inputtextbox = form.InputText.Text;
+        }
+
+
+        private void TextChanged(object sender, RoutedEventArgs e)
+        {
+            if (ChatBox.Text.Length > 500)
+            {
+                var remove = ChatBox.Text.Remove(0, 450);
+                //ChatBox.Clear();
+            }
+            ChatBox.ScrollToEnd();
+            InputText.Clear();
+        }
+
+        private void KeydownAction(object sender, KeyEventArgs keyargs)
+        {
+            switch (keyargs.Key)
+            {
+                case Key.Enter:
+                    TextPopulated = true;
+                    ChatBoxProvider(InputText.Text);
+                    break;
+                // only triggers when textbox is focused
+                case Key.F12:
+                    ConsoleManager.ConsoleInitalize();
+                    break;
+            }
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextPopulated = true;
+            ChatBoxProvider(InputText.Text); 
+        }
+
+        private void ChatBoxProvider(string message)
+        {
+            if (_textpos == 0)
+            {
+                ChatBox.Text += message;
+                _textpos = 1;
+            }else ChatBox.Text += $"\n{message}";
+        }
+        
+        internal static void Message(string message)
+        {
+            form.ChatBoxProvider(message);
+        }
+
+    }
+    class OSCData
+    {
+        private static List<string> _pathBlacklist = new List<string>()
+        {
+            "/avatar/parameters/IsLocal",
+            "/avatar/parameters/Voice",
+            "/avatar/parameters/VelocityX",
+            "/avatar/parameters/VelocityY",
+            "/avatar/parameters/VelocityZ",
+            "/avatar/parameters/AngularX",
+            "/avatar/parameters/AngularY",
+            "/avatar/parameters/AngularZ",
+            "/avatar/parameters/Viseme",
+            "/avatar/parameters/Grounded",
+            "/avatar/parameters/Upright",
+            "/avatar/parameters/GestureLeft",
+            "/avatar/parameters/GestureRight",
+            "/avatar/parameters/GestureLeftWeight",
+            "/avatar/parameters/GestureRightWeight",
+            "/avatar/parameters/Seated",
+            "/avatar/parameters/MuteSelf",
+            "/avatar/parameters/InStation",
+            "/avatar/parameters/TrackingType"
+        };
+
+        static OscReceiver _receiver;
+        static OscSender _sender;
+        private static bool _isallowed;
+
+
+
+        internal static void start()
+        {
+            static void InvokeMessageOnMainThread(string message)
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    MainWindow.Message(message);
+                });
+            }
+            _sender = new OscSender(IPAddress.Parse("127.0.0.1"), 0, 9000);
+            _receiver = new OscReceiver(9001);
+
+            _sender.Connect();
+            
+
+            // Create a thread to do the listening
+            var threadlisten = new Thread(ListenLoop);
+
+            var chatthread = new Thread(ChatLoop);
+
+            // Connect the receiver
+            _receiver.Connect();
+            
+            // start threads
+            threadlisten.Start();
+            chatthread.Start();
+
+            switch (_sender.State)
+            {
+                case OscSocketState.Connected:
+                    Console.WriteLine("Connected");
+                    InvokeMessageOnMainThread("Connected");
+                    _isallowed = true;
+                    break;
+                case OscSocketState.NotConnected:
+                    Console.WriteLine("Not Connected");
+                    InvokeMessageOnMainThread("Not Connected");
+                    _isallowed = false;
+                    break;
+                case OscSocketState.Closing:
+                    Console.WriteLine("Connection Closing");
+                    InvokeMessageOnMainThread("Connection Closing");
+                    _isallowed = false;
+                    break;
+                case OscSocketState.Closed:
+                    Console.WriteLine("Connection Closed");
+                    InvokeMessageOnMainThread("Connection Closed");
+                    _isallowed = false;
+                    break;
+            }
+
+
+            static void ChatLoop()
+            {
+                try
+                {
+                    while (_isallowed = true)
+                    {
+                        if (MainWindow.TextPopulated)
+                        {
+                            _sender.Send(new OscMessage("/chatbox/input", MainWindow.Inputtextbox, true));
+                            MainWindow.TextPopulated = false;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+            // receiver listen loop
+            static void ListenLoop()
+            {
+                try
+                {
+                    while (_receiver.State != OscSocketState.Closed)
+                    {
+                        // if we are in a state to recieve
+                        if (_receiver.State == OscSocketState.Connected)
+                        {
+                            OscPacket packet = _receiver.Receive();
+                            var message = (OscMessage) packet;
+                            
+                            Console.WriteLine(packet.ToString());
+                            InvokeMessageOnMainThread(packet.ToString());
+                            
+                            if (!_pathBlacklist.Contains(message.Address)) InvokeMessageOnMainThread(packet.ToString());
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.Write(ex.ToString());
+                }
+            }
+
+        }
+    }
+}
