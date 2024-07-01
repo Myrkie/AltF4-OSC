29f6fd385e48f9a8a4c060b857b2393a7c427d5d
Myrkur <Myrkur@Myrkur.dev>
2024-07-01 16:09:23 -0400

    project repurpose => AltF4 OSC

---
+++ AltF4 OSC/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows;
+
+namespace WPF_OSC_Keyboard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private static MainWindow _instance;
+
+        public static MainWindow Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new MainWindow();
+                }
+                return _instance;
+            }
+        }
+        public MainWindow()
+        {
+            _instance = this;
+            InitializeComponent();
+            OSCData.start();
+        }
+        
+        
+        public bool IsCheckBoxChecked
+        {
+            get
+            {
+                bool isChecked = false;
+                Dispatcher.Invoke(() =>
+                {
+                    isChecked = EnableCheckBox.IsChecked ?? false;
+                });
+                return isChecked;
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            OSCData.Dispose();
+            Application.Current.Shutdown();
+        }
+
+        private void ConsoleButton_Click(object sender, RoutedEventArgs e)
+        {
+            ConsoleManager.ConsoleInitalize();
+        }
+
+        private void ClearBox(object sender, RoutedEventArgs e)
+        {
+            NotificationBox.Clear();
+        }
+
+        private void NotifBoxProvider(string message)
+        {
+            NotificationBox.AppendText($"{message}\n");
+            if (AutoScroll.IsChecked == true)
+            {
+                NotificationBox.ScrollToEnd();
+            }
+        }
+        
+        internal static void Message(string message)
+        {
+            _instance.NotifBoxProvider(message);
+        }
+    }
+}
---
+++ AltF4 OSC/OSCData.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows;
+using VRC.OSCQuery;
+using VRChatOSCLib;
+
+namespace WPF_OSC_Keyboard;
+
+public static class OSCData
+{
+    static VRChatOSC osc = new();
+
+    internal static void Dispose()
+    {
+        osc.Dispose();
+        service.Dispose();
+    }
+
+    static void InvokeMessageOnMainThread(string message)
+    {
+        Application.Current.Dispatcher.Invoke(() => { MainWindow.Message(message); });
+    }
+
+    private static OSCQueryService service;
+
+    internal static void start()
+    {
+        service = new OSCQueryServiceBuilder().WithServiceName("AltF4 OSC").WithUdpPort(Extensions.GetAvailableUdpPort()).WithTcpPort(Extensions.GetAvailableTcpPort()).WithDefaults().Build();
+        
+        osc.OnConnectionStateChanged += OnConnectionStateChanged;
+        osc.Connect();
+        osc.Listen();
+        osc.OnMessage += OnMessageReceived;
+    }
+    
+    static void OnMessageReceived(object? source, VRCMessage message)
+    {
+        message.Print();
+        
+        VRCMessage.MessageType messageType = message.Type;
+
+        if (messageType != VRCMessage.MessageType.AvatarParameter ||
+            message.AvatarParameter != "Misc/Disconnect") return;
+        object? value = message.GetValue();
+        if (value is bool booleanValue)
+        {
+            if (booleanValue)
+            {
+                InvokeMessageOnMainThread($"{message.AvatarParameter} | State: true");
+                    
+                if (MainWindow.Instance.IsCheckBoxChecked)
+                { 
+                    InvokeMessageOnMainThread("Conditions met, closing vrchat");
+                    YeetVrc("vrchat");
+                }
+            }
+            else
+            {
+                InvokeMessageOnMainThread($"{message.AvatarParameter} | State: false");
+            }
+        }
+    }
+
+
+    private static void OnConnectionStateChanged(object? sender, ConnectionState state)
+    {
+        Console.WriteLine($"Connection state changed to: {state}");
+
+        switch (state)
+        {
+            case ConnectionState.NotConnected:
+                InvokeMessageOnMainThread("Connection state changed to: NotConnected");
+                break;
+            case ConnectionState.Connecting:
+                InvokeMessageOnMainThread("Connection state changed to: Connecting");
+                break;
+            case ConnectionState.Connected:
+                InvokeMessageOnMainThread("Connection state changed to: Connected");
+                break;
+            case ConnectionState.Closing:
+                InvokeMessageOnMainThread("Connection state changed to: Closing");
+                break;
+            case ConnectionState.Closed:
+                InvokeMessageOnMainThread("Connection state changed to: Closed");
+                break;
+        }
+    }
+    private static void YeetVrc(string processName)
+    {
+        int delayMilliseconds = 1000;
+
+        var processes = Process.GetProcessesByName(processName);
+
+        if (processes.Length == 1)
+        {
+            Process process = processes[0];
+            InvokeMessageOnMainThread($"Found process: {process.ProcessName}, PID: {process.Id}");
+            Thread.Sleep(delayMilliseconds);
+
+            try
+            {
+                process.CloseMainWindow();
+                InvokeMessageOnMainThread($"Process {process.ProcessName} terminated.");
+            }
+            catch (Exception ex)
+            {
+                InvokeMessageOnMainThread($"Failed to terminate process {process.ProcessName}: {ex.Message}");
+            }
+        }
+    }
+}
